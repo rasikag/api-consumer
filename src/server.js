bd2172dd15950d7f60173893fbb099c76005bad1
rasikag <rasikagayan90@gmail.com>
2018-11-09 01:18:59 +0800

    pushing hereku

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
-import express from "express";
+const express = require('express');
+const http = require('http')
+const path = require('path');
+
 const app = express();
 
-app.use(static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));

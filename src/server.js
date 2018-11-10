1111dec2279379e6008efc6517412d64520fd106
rasikag <rasikagayan90@gmail.com>
2018-11-10 13:49:25 +0800

    heroku deployment

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(__dirname +'/dist/'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
